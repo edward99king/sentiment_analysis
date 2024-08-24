c7980e4460b885879d450f0b0f2486de1fbd97a8
unknown <edward99king@gmail.com>
2024-08-24 17:10:14 +0700

    Push new code

--- utils/connection.py
+++ utils/connection.py
@@ -1,10 +1,10 @@
 import sqlite3 as sq
 from utils import common_utils
-
+import os
 
 def get_sql_connection():
     try:
-        db = sq.connect(common_utils.get_current_directory() + '\im.db')
+        db = sq.connect(os.path.join(common_utils.get_current_directory(),'im.db'))
 
         return db
     except Exception as ex:
