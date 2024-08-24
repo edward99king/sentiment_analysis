4719b4d6515e07d0cbc3f6d5f4226d908462d128
unknown <edward99king@gmail.com>
2024-08-24 17:00:48 +0700

    Push new code

--- utils/connection.py
+++ utils/connection.py
@@ -3,6 +3,10 @@ from utils import common_utils
 
 
 def get_sql_connection():
-    db = sq.connect(common_utils.get_current_directory() + '\im.db')
+    try:
+        db = sq.connect(common_utils.get_current_directory() + '\im.db')
 
-    return db
+        return db
+    except Exception as ex:
+        print(ex)
+        return None
